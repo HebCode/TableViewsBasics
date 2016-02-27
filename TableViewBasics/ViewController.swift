1d4c04d098852f5f5dae9a1d8b4baeaf49f0ca69
Eran Guttentag <gutte@mail.com>
2016-02-27 22:47:54 +0200

    finished the data source stage

--- TableViewBasics/ViewController.swift
+++ TableViewBasics/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let cities = ["Jerusalem", "Tel-Aviv Yaffo", "Haifa", "Be'er Sheva", "Rishon Letsion", "Ashdod", "Ashkelon", "Kazerin", "Kiryat Shemona", "Eilat", "Ramat Gan", "Givatayim", "Arad", "Rehovot"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cityCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "cityCell")
+        }
+        cell!.textLabel?.text = cities[indexPath.row]
+        cell!.detailTextLabel?.text = indexPath.row.description
+        return cell!
+    }
 }
 
